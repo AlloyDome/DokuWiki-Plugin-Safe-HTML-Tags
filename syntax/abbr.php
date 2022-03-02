7049e8fb96261aaf9b5d6efda63c54b95524b684
AlloyDome <77043817+AlloyDome@users.noreply.github.com>
2022-03-02 19:38:57 +0800

    Add files via upload

---
+++ syntax/abbr.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 
+ *
+ * @license	MIT License
+ * @author	
+ * 
+ * @since	1.0.0, beta (------)
+ * @version 1.0.0, beta (------)
+ */
+
+if(!defined('DOKU_INC'))
+	die('It works!');	// 必须在 Dokuwiki 下运行 · Must be run within Dokuwiki
+
+require_once(__DIR__ . '/../inc/init.php');
+
+class syntax_plugin_safehtmltags_abbr extends syntax_plugin_GeneralSyntax {
+	public $tagName = 'abbr';
+	public $isCouple = true;
+	public $isSingle = false;
+
+	public function getType() {
+		return 'substition';
+	}
+
+	public function getPType() {
+		return 'normal';
+	}
+}
